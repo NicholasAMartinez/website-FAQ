2734990c37bbaf285e0e98a6c51500cff5130fe1
Cameron <cameronlillis52@gmail.com>
2026-01-05 15:00:45 -0800

    Finished scrolling fixed header.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import axios from "axios";
-import "./styles.css"
+import "./styles.css";
 import { useState } from "react";
 import Header from "./components/ui/Header";
 import Faq from "./components/ui/FAQ/faq";
@@ -24,38 +24,38 @@ export default function Home() {
 
   return (
     <>
-      <Header />
       <main>
-        <section className="hero-section">
-          <div className="hero-title">
-            <div className="rebelhacks">
-              <h1 className="rebel">REBEL</h1>
-              <h1 className="hacks">HACKS</h1>
-              <h2 className="unlv">UNLV 2026</h2>
+        <section id="home" className="hero-section">
+          <Header />
+          <div className="hero-wrapper">
+            <div className="hero-title">
+              <h1 className="title-org">REBEL</h1>
+              <h1 className="title-org">HACKS</h1>
+              <h2 className="title-school">UNLV 2026</h2>
+
+              <p className="hero-description">
+                Come together to design, develop, and present projects that
+                tackle real-world problems!
+              </p>
+
+              <p className="hero-date">FRI & SAT February 20–21, 2026</p>
             </div>
 
-            <p className="hero-description">
-              Come together to design, develop, and present projects that tackle
-              real-world problems!
-            </p>
-
-            <p className="hero-date">FRI & SAT February 20–21, 2026</p>
-          </div>
-
-          <div className="hero-image-wrapper">
-            <Image
-              src="/images/vegas-sign.svg"
-              alt="Las Vegas neon sign"
-              width={580}
-              height={100}
-              className="hero-image"
-            />
+            <div className="hero-image-wrapper">
+              <Image
+                src="/images/vegas-sign.svg"
+                alt="Las Vegas neon sign"
+                width={580}
+                height={100}
+                className="hero-image"
+              />
+            </div>
           </div>
         </section>
 
-        {/* <section className="faq">
-        <Faq />
-      </section> */}
+        <section id="about" className="faq">
+          <Faq />
+        </section>
       </main>
     </>
   );
